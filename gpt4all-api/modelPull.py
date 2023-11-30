a31a150aeb0d5c4877492605b8ea109fcd3dbe8d
OussamaFajrElidrissiPXL <57445216+OussamaFajrElidrissiPXL@users.noreply.github.com>
2023-11-30 11:25:04 +0100

    Added modelPull.py to download model into the right folder
    Python script that download the LLM (zephyr-7b-alpha.Q4_K_M.gguf) into the right folder (gpt4pxl\gpt4all-api\models)

---
+++ gpt4all-api/modelPull.py
@@ -0,0 +1,17 @@
+from huggingface_hub import hf_hub_download
+import os
+
+# Specify the filename to download
+selected_filename = "zephyr-7b-alpha.Q4_K_M.gguf"
+
+directory = os.getcwd()
+destination_path = os.path.join(directory, "models", selected_filename)
+
+# Check if the file already exists in the destination folder
+if os.path.exists(destination_path):
+    print(f"File '{selected_filename}' already exists in the /models folder.")
+else:
+    # Download the selected file
+    print(f"Downloading '{selected_filename}'.")
+    hf_hub_download(repo_id="TheBloke/zephyr-7B-alpha-GGUF", filename=selected_filename, local_dir=directory + "/models", local_dir_use_symlinks=False)
+    print(f"File '{selected_filename}' downloaded successfully into the /models folder.")
